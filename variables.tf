d6d4bbaadc007da42e4cb3d2cd791838925ca98b
jgerson <jeremy.gerson@gmail.com>
2018-03-06 14:02:06 -0500

    adjusting region and ami

--- variables.tf
+++ variables.tf
@@ -1,16 +1,16 @@
 variable "aws_region" {
   description = "AWS region"
-  default = "us-west-1"
+  default = "us-east-1"
 }
 
 variable "ami_id" {
   description = "ID of the AMI to provision. Default is Ubuntu 14.04 Base Image"
-  default = "ami-2e1ef954"
+  default = "ami-97785bed"
 }
 
 variable "instance_type" {
   description = "type of EC2 instance to provision."
-  default = "t2.micro"
+  default = "t2.medium"
 }
 
 variable "name" {
